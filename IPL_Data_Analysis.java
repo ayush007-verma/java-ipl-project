0b14ff123f2f003a290f011af47942846cce9d70
Ayush <ayushpersonal2310@gmail.com>
2023-09-03 19:56:20 +0530

    setup environment for ipl-project

---
+++ IPL_Data_Analysis.java
@@ -0,0 +1,8 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class IPL_Data_Analysis {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
